2f43acd25bcdc892346b9d5c893ca96c684de135
muqadas tahir <muqadastahirmt@gmail.com>
2026-03-06 23:02:11 +0500

    Implemented login feature

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello Secure Software Development")
+users = {
+    "user1": "pass123",
+    "user2": "mypassword"
+}
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username in users and users[username] == password:
+    print("Login successful! Welcome,", username)
+else:
+    print("Login failed! Check username and password.")
